50c8a5fc83c6a9c01aa19fa7df34c45898dd70e8
jmaassen <J.Maassen@esciencecenter.nl>
2013-10-16 17:48:25 +0200

    Initial commit of AetherStackingMulti. Based on https://gforge.cs.vu.nl/svn/ibis/ipl/trunk rev. 15176

---
+++ src/nl/esciencecenter/aether/impl/stacking/multi/AetherIdentifierWrapper.java
@@ -0,0 +1,23 @@
+package nl.esciencecenter.aether.impl.stacking.multi;
+
+import nl.esciencecenter.aether.AetherIdentifier;
+
+final class AetherIdentifierWrapper implements Comparable<AetherIdentifierWrapper> {
+    final AetherIdentifier id;
+    final String ibisName;
+
+    AetherIdentifierWrapper(String ibisName, AetherIdentifier id) {
+        this.id = id;
+        this.ibisName = ibisName;
+    }
+
+    public int compareTo(AetherIdentifierWrapper w) {
+        int compare = this.ibisName.compareTo(w.ibisName);
+        if (compare == 0) {
+            return id.compareTo(w.id);
+        }
+        else {
+            return compare;
+        }
+    }
+}
---
+++ src/nl/esciencecenter/aether/impl/stacking/multi/MultiAetherProperties.java
@@ -0,0 +1,11 @@
+package nl.esciencecenter.aether.impl.stacking.multi;
+
+import nl.esciencecenter.aether.AetherProperties;
+
+public interface MultiAetherProperties {
+    public static final String PREFIX = AetherProperties.PREFIX + "multi.";
+    public static final String STARTERS = PREFIX + "implementations";
+    public static final String IMPLEMENTATION_JARS = PREFIX + "jars.";
+    public static final String PROPERTIES = PREFIX + "properties.";
+    public static final String PROPERTIES_FILE = PREFIX + "propertiesfile.";
+}
